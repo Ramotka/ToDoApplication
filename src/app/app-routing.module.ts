d551602d1e5528dcd103efd29405145b71cf9aec
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-30 22:16:00 +0200

    Header and myListPage added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyListPageModule } from './pages/my-list.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: '', 
+        loadChildren: () => MyListPageModule
+      }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
