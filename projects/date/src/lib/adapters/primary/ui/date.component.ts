7a6bbfc5baeed456f365c6c83fcbd7a15f0397e4
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-05 19:16:06 +0200

    new Date() works (2 versions)

--- projects/date/src/lib/adapters/primary/ui/date.component.ts
+++ projects/date/src/lib/adapters/primary/ui/date.component.ts
@@ -1,5 +1,21 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from "@angular/core";
+import { Observable, of } from "rxjs";
+import { DateDTO } from "../../../application/ports/secondary/date.dto";
 
-@Component({ selector: 'lib-date', templateUrl: './date.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: "lib-date",
+  templateUrl: "./date.component.html",
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class DateComponent {
+  todayDate$: Observable<DateDTO> = of({
+    currentDate: new Date(),
+  });
+  //   Ktora wersja lespza?
+  dateToday = new Date();
 }
