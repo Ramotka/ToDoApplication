a7670c7077afaa92daeaa141c3cf6da30571bc4f
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-10 21:46:14 +0200

    Removing tasks functionality added

--- projects/tasks/src/lib/adapters/primary/ui/tasks-list-view.component.ts
+++ projects/tasks/src/lib/adapters/primary/ui/tasks-list-view.component.ts
@@ -1,3 +1,4 @@
+import { BsDropdownConfig } from "ngx-bootstrap/dropdown";
 import {
   Component,
   ViewEncapsulation,
@@ -10,7 +11,10 @@ import {
   GETS_ALL_TASK_DTO,
   GetsAllTaskDtoPort,
 } from "../../../application/ports/secondary/gets-all-task.dto-port";
-import { BsDropdownConfig } from "ngx-bootstrap/dropdown";
+import {
+  REMOVES_TASK_DTO,
+  RemovesTaskDtoPort,
+} from "../../../application/ports/secondary/removes-task.dto-port";
 
 @Component({
   selector: "lib-tasks-list-view",
@@ -28,6 +32,11 @@ export class TasksListViewComponent {
   tasksList$: Observable<TaskDTO[]> = this._getsAllTaskDto.getAll();
 
   constructor(
-    @Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort
+    @Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort,
+    @Inject(REMOVES_TASK_DTO) private _removesTaskDto: RemovesTaskDtoPort
   ) {}
+
+  onDeleteTaskClicked(task: Partial<TaskDTO>): void {
+    this._removesTaskDto.remove("" + task.id);
+  }
 }
