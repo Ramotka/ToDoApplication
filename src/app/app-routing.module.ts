fc0758752fb40071e74ae34f43bde7f5048844b5
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-15 16:21:24 +0200

    redundant page deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,29 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyListPageModule } from './pages/my-list.page-module';
 import { HomePageModule } from './pages/home.page-module';
-import { AddTaskPageModule } from './pages/add-task.page-module';
 import { FirstTaskPageModule } from './pages/first-task.page-module';
 
-const routes: Routes = [{ 
-        path: 'my-list', 
-        loadChildren: () => MyListPageModule
-      },
-  { 
-        path: '', 
-        loadChildren: () => HomePageModule
-      },
-  { 
-        path: 'add-task', 
-        loadChildren: () => AddTaskPageModule
-      },
-  { 
-        path: 'first-task', 
-        loadChildren: () => FirstTaskPageModule
-      }
+const routes: Routes = [
+  {
+    path: 'my-list',
+    loadChildren: () => MyListPageModule,
+  },
+  {
+    path: '',
+    loadChildren: () => HomePageModule,
+  },
+  {
+    path: 'first-task',
+    loadChildren: () => FirstTaskPageModule,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
